3942a11e00345933128918f5fcf23607acf00c3e
AlbertoEnsiklope <makakitomakako69@gmail.com>
2024-07-24 22:29:35 +0200

    Update comienzo.sh

--- muyUtil/comienzo.sh
+++ muyUtil/comienzo.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 mensaje="ACCEDER A Chrome Remote Desktop Access: https://remotedesktop.google.com RECORDAR PIN: 123456"
 cd ~
-# CURRENT_USER=$(logname)
-# sudo usermod -aG sudo "$CURRENT_USER"
 
 sudo apt-get update
 sudo apt-get update --fix-missing
@@ -33,50 +31,31 @@ sudo apt install -y unzip
 
 echo "$mensaje"
 
-# rm -f chrome-remote-desktop_current_amd64.deb
-# rm -f firefox.tar.bz2
-
-# echo "Archivos descargados eliminados."
-# echo "Script completado."
-
 echo "$mensaje"
-
-# sudo apt-get autoremove
-# sudo apt-get --purge remove && sudo apt-get autoclean
-# sudo apt-get -f install
-# sudo apt-get update
-# sudo apt-get upgrade && sudo apt-get dist-upgrade
 
 sudo useradd -m -s /bin/bash franco
 echo "franco:vivaspain" | sudo chpasswd
 
 sudo usermod -aG sudo franco
 
-# test1
 sudo apt-get -y --fix-broken install
-# test2
 sudo apt-get -y update --fix-missing
 
 sudo dpkg-reconfigure -a
 sudo dpkg --configure -a
 
-# Instalar PulseAudio
 sudo apt-get install -y pulseaudio
 
-# Iniciar PulseAudio
 pulseaudio --start
 
-# Configurar PulseAudio para permitir el acceso a la red
 CONFIG_FILE="/etc/pulse/default.pa"
 TEMP_FILE="/tmp/default.pa"
 
-# Añadir las líneas necesarias al archivo de configuración
 sudo cp $CONFIG_FILE $TEMP_FILE
 echo "load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1;192.168.0.0/24" | sudo tee -a $TEMP_FILE
 echo "load-module module-esound-protocol-tcp auth-ip-acl=127.0.0.1;192.168.0.0/24" | sudo tee -a $TEMP_FILE
 sudo mv $TEMP_FILE $CONFIG_FILE
 
-# Reiniciar PulseAudio
 pulseaudio --kill
 pulseaudio --start
 
@@ -86,6 +65,3 @@ echo "$mensaje"
 echo "$mensaje"
 echo "$mensaje"
 echo "$mensaje"
-
-# echo "1a Presiona cualquier tecla para continuar... 1a"
-# read -n 1 -s
